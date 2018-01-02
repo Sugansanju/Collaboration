a6598cef06e4c88737f99d95df1e822be1619fec
Sugansanju <suganjo15@gmail.com>
2018-01-02 16:20:45 +0530

    Common Navbar

--- onlinesite/WebContent/assets/js/app.js
+++ onlinesite/WebContent/assets/js/app.js
@@ -1,7 +1,1 @@
 var app = angular.module('module',["ngRoute","ngStorage","ngCookies"]);
-
-app.directive('navBar',function(){
-	return{
-		templateUrl:"../ui/navbar.html"
-	}
-});
--- onlinesite/WebContent/assets/js/app.routes.js
+++ onlinesite/WebContent/assets/js/app.routes.js
@@ -1,5 +1,10 @@
 var app = angular.module('module',["ngRoute","ngStorage","ngCookies"]);
 
+app.directive('navBar',function(){
+	return{
+		templateUrl:"./ui/navbar.html"
+	}
+});
 app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
 	$routeProvider.
 when('/blog',{
